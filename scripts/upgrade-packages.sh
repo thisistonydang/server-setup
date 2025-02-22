0f749f7f76649af29b9515d153853e95a17de274
Tony Dang <tony@tonydang.com>
2025-02-22 07:21:00 -0800

    add doc

--- scripts/upgrade-packages.sh
+++ scripts/upgrade-packages.sh
@@ -2,6 +2,8 @@
 set -euo pipefail
 
 # Retrieves the latest package lists.
+# NOTE: `apt-get` is used instead of `apt` because it has a stable CLI API and
+# guaranteed backward compatibility.
 apt-get update
 
 # Upgrades all installed packages. The --yes flag avoid manual confirmation.
