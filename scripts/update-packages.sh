c2e9a2f292b8d2c31cd89e4d77d86d21aff4b886
Tony Dang <tony@tonydang.com>
2025-02-22 07:12:05 -0800

    add package upgrade script

--- scripts/update-packages.sh
+++ scripts/update-packages.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 set -euo pipefail
+
+# Retrieves the latest package lists.
+apt-get update
+
+# Upgrades all installed packages. The --yes flag avoid manual confirmation.
+apt-get upgrade --yes
+
+# Reboot server. Some updates require a reboot to take effect.
+reboot
