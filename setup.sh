877e0424b1bcb02ac3a3111483d0c5567762c517
Tony Dang <tony@tonydang.com>
2025-02-22 06:43:11 -0800

    add step to update packages

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
-# Load environment variables
+# Load environment variables.
 source .env
 
-# Configure ufw firewall
+# Configure ufw firewall.
 ssh root@${IP} bash < ./scripts/configure-firewall.sh
 
-# Create non-root user with sudo privileges
+# Create non-root user with sudo privileges.
 ssh root@${IP} bash < ./scripts/create-user.sh
+
+# Update and upgrade packages.
+ssh root@${IP} bash < ./scripts/update-packages.sh
