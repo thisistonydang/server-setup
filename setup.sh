1d4958ed35aaa68803aa816729da5216caddb2b8
Tony Dang <tony@tonydang.com>
2025-02-22 07:10:29 -0800

    update doc

--- setup.sh
+++ setup.sh
@@ -10,5 +10,5 @@ ssh root@${IP} bash < ./scripts/configure-firewall.sh
 # Create non-root user with sudo privileges.
 ssh root@${IP} bash < ./scripts/create-user.sh
 
-# Update and upgrade packages.
+# Upgrade packages and reboot system.
 ssh root@${IP} bash < ./scripts/update-packages.sh
