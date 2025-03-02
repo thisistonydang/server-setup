fb1d6bdb3fcc0be2549b25b1cdd79df5cb8976ad
Tony Dang <tony@tonydang.com>
2025-03-01 17:23:57 -0800

    copy .env to server

--- setup.sh
+++ setup.sh
@@ -4,6 +4,10 @@ set -euo pipefail
 # Load environment variables.
 source .env
 
+# Copy .env file to remote server.
+rsync -avLP .env root@${IP}:/root
+echo ".env file copied to remote server."
+
 # Configure ufw firewall.
 ssh root@${IP} bash < ./scripts/configure-firewall.sh
 
