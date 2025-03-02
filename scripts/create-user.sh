f3e3487f8aac76f138c029d1b6fd67f9584140d3
Tony Dang <tony@tonydang.com>
2025-03-01 18:02:22 -0800

    create sudo user

--- scripts/create-user.sh
+++ scripts/create-user.sh
@@ -8,4 +8,8 @@ source .env
 if id "${USERNAME}" &>/dev/null; then
     echo "User '${USERNAME}' already exists."
 else
+    # Create a new user with sudo privileges.
+    useradd --create-home --shell /bin/bash --groups sudo ${USERNAME}
+    echo "User '${USERNAME}' created with sudo privileges."
+
 fi
