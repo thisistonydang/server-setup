4d44f174408252a6bbe73a27a1ff3fe455d1aa55
Tony Dang <tony@tonydang.com>
2025-03-01 18:01:54 -0800

    git cm error

--- scripts/create-user.sh
+++ scripts/create-user.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 # Load environment variables. Required so that the USERNAME variable is available.
 source .env
 
-fi
 # Only create a new user if one doesn't already exist.
 if id "${USERNAME}" &>/dev/null; then
     echo "User '${USERNAME}' already exists."
