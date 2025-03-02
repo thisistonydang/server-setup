8d68b4e59630f4f4be790fee03a48f13f63f229f
Tony Dang <tony@tonydang.com>
2025-03-02 11:38:44 -0800

    add echo status

--- scripts/upgrade-packages.sh
+++ scripts/upgrade-packages.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
+echo "Upgrading packages..."
+
 # Retrieves the latest package lists.
 # NOTE: `apt-get` is used instead of `apt` because it has a stable CLI API and
 # guaranteed backward compatibility.
@@ -8,6 +10,8 @@ apt-get update
 
 # Upgrades all installed packages. The --yes flag avoid manual confirmation.
 apt-get upgrade --yes
+
+echo ""
 
 # Reboot server. Some updates require a reboot to take effect.
 echo "Rebooting server...Goodbye!"
