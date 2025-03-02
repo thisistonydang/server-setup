fe45da08bb182f73fb3c43c1b8fceb7a058809fb
Tony Dang <tony@tonydang.com>
2025-03-01 18:01:26 -0800

    check if user already exists before creation

--- scripts/create-user.sh
+++ scripts/create-user.sh
@@ -4,3 +4,9 @@ set -euo pipefail
 # Load environment variables. Required so that the USERNAME variable is available.
 source .env
 
+fi
+# Only create a new user if one doesn't already exist.
+if id "${USERNAME}" &>/dev/null; then
+    echo "User '${USERNAME}' already exists."
+else
+fi
