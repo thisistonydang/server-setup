b63595fcfe9fc04ff5ce1c0d7002b5ccb3aea5fe
Tony Dang <tony@tonydang.com>
2025-03-02 11:56:11 -0800

    add ssh configuration logic

--- scripts/configure-ssh.sh
+++ scripts/configure-ssh.sh
@@ -3,3 +3,38 @@ set -euo pipefail
 
 echo "Configuring SSH..."
 
+# Verify PasswordAuthentication setting is recognized by sshd (in case the setting was deprecated).
+if ! sshd -T | grep --quiet "^passwordauthentication\s\+" ; then
+    echo "Warning: PasswordAuthentication setting not recognized by sshd"
+    exit 1
+fi
+
+# Change PasswordAuthentication setting to `no` if it exists. 
+sed --in-place 's/^#*\(PasswordAuthentication\) .*/\1 no/' /etc/ssh/sshd_config
+
+# If PasswordAuthentication setting doesn't exist yet, add it to the file.
+grep --quiet '^PasswordAuthentication no' /etc/ssh/sshd_config || echo 'PasswordAuthentication no' >> /etc/ssh/sshd_config
+
+# Verify PermitRootLogin setting is recognized by sshd (in case the setting was deprecated).
+if ! sshd -T | grep --quiet "^permitrootlogin\s\+" ; then
+    echo "Warning: PermitRootLogin setting not recognized by sshd"
+    exit 1
+fi
+
+# Change PermitRootLogin setting to prohibit-password if it exists.
+sed --in-place 's/^#*\(PermitRootLogin\) .*/\1 prohibit-password/' /etc/ssh/sshd_config
+
+# If PermitRootLogin setting doesn't exist yet, add it to the file.
+grep --quiet '^PermitRootLogin prohibit-password' /etc/ssh/sshd_config || echo 'PermitRootLogin prohibit-password' >> /etc/ssh/sshd_config
+
+# Restart SSH service.
+systemctl restart ssh
+
+echo "SSH configured!"
+
+# Verify settings were applied.
+sshd -T | grep --extended-regexp --ignore-case 'PasswordAuthentication|PermitRootLogin'
+echo ""
+
+# Reference:
+# https://man.openbsd.org/sshd_config
