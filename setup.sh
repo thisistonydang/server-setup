023bc513c07a8dacf4f8f13f4cc73ac51f44045f
Tony Dang <tony@tonydang.com>
2025-03-02 06:50:57 -0800

    copy .env to server

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 # Load environment variables. Required so that the IP variable is available.
 source .env
 
+# Copy the .env file to the server so that it can be used by below scripts.
+rsync -avLP .env root@${IP}:/root
+
 # Configure ufw firewall.
 ssh root@${IP} bash < ./scripts/configure-firewall.sh
 
