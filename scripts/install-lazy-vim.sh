bd3ae5826ba69d67968df4edb1c9fac4ca496e96
Tony Dang <tony@tonydang.com>
2025-03-09 14:40:29 -0700

    add LazyVim requrements

--- scripts/install-lazy-vim.sh
+++ scripts/install-lazy-vim.sh
@@ -6,3 +6,17 @@ echo "Installing LazyVim..."
 # Load environment variables. Required so that the USERNAME variable is available.
 source .env
 
+# Add LazyVim requirements if nvim is not already installed.
+if [[ ! -d ~/.config/nvim ]]; then
+    # Add PPA to get the latest version of Neovim.
+    echo "Adding PPA for latest stable Neovim..."
+    add-apt-repository --yes ppa:neovim-ppa/stable
+    apt-get update
+    echo ""
+
+    # Install bare LazyVim requirements.
+    echo "Installing LazyVim requirements..."
+    apt-get install --yes neovim build-essential
+    echo ""
+fi
+
