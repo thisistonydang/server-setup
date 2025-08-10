9b0c1d284c9b9cda5bd60c5cab052ed6041bba8f
Tony Dang <tony@tonydang.com>
2025-08-10 14:48:37 -0700

    copy dotfiles if DOTFILES_REPO is set

--- scripts/install-zsh.sh
+++ scripts/install-zsh.sh
@@ -10,6 +10,13 @@ source .env
 if ! command -v zsh &> /dev/null; then
     apt-get update
     apt-get install --yes zsh
+
+    if [[ -n "${DOTFILES_REPO}" ]]; then
+        git clone --depth=1 https://github.com/${DOTFILES_REPO}.git /home/${USERNAME}/repos/${DOTFILES_REPO}
+        chown --recursive ${USERNAME}:${USERNAME} /home/${USERNAME}/repos/${DOTFILES_REPO}
+        ln --symbolic /home/${USERNAME}/repos/${DOTFILES_REPO}/.zshrc /home/${USERNAME}/.zshrc
+        ln --symbolic /home/${USERNAME}/repos/${DOTFILES_REPO}/.gitconfig /home/${USERNAME}/.gitconfig
+    fi
 fi
 
 # Change default shell to ZSH for user.
