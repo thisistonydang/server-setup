a3bc45c3cbda8bd33570c943f5069e4f9d696f9d
Tony Dang <tony@tonydang.com>
2026-01-18 15:36:53 -0800

    clean remote backups

--- scripts/backup-db-clean-up.sh
+++ scripts/backup-db-clean-up.sh
@@ -12,3 +12,7 @@ MIN_AGE_IN_DAYS=30
 
 # Clean local backups.
 find "${POSTGRES_LOCAL_BACKUP_DIR}" -type f -mtime +${MIN_AGE_IN_DAYS} -delete
+
+# Clean remote backups.
+rclone delete "${POSTGRES_REMOTE_BACKUP_DIR}" --min-age ${MIN_AGE_IN_DAYS}d
+
