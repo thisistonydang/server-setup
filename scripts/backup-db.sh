369f982f664591b3a4ff0e7dcde3cfbcba9e215a
Tony Dang <tony@tonydang.com>
2026-01-18 14:20:54 -0800

    load .env

--- scripts/backup-db.sh
+++ scripts/backup-db.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 set -euo pipefail
+
+# Load environment variables. Required so that the PostgreSQL related variables are available:
+# - POSTGRES_HOST
+# - POSTGRES_PORT
+# - POSTGRES_DB
+# - POSTGRES_USER
+source .env
+
