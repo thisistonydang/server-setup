89b1d9333c0a696bbdf45656c6c54defe395ed34
Tony Dang <tony@tonydang.com>
2026-01-18 15:27:43 -0800

    load .env

--- scripts/backup-db-clean-up.sh
+++ scripts/backup-db-clean-up.sh
@@ -3,3 +3,7 @@ set -euo pipefail
 
 echo "Cleaning up old database backups..."
 
+# Load environment variables. Required so that the following variables are available:
+# - POSTGRES_LOCAL_BACKUP_DIR
+# - POSTGRES_REMOTE_BACKUP_DIR
+source .env
