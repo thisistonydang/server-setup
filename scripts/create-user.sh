79a69626c9255fa027e9a8b1a4e0cb50733c0f71
Tony Dang <tony@tonydang.com>
2026-01-20 08:28:01 -0800

    more robust path to .env

--- scripts/create-user.sh
+++ scripts/create-user.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 echo "Creating user..."
 
 # Load environment variables. Required so that the USERNAME variable is available.
-source .env
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "${SCRIPT_DIR}/../.env"
 
 # Only create a new user if one doesn't already exist.
 if id "${USERNAME}" &>/dev/null; then
