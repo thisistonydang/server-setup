75316e8cf49773c820fc4e1bd4b242df2d666b6a
Tony Dang <tony@tonydang.com>
2026-01-21 08:12:02 -0800

    format

--- scripts/install-lazy-vim.sh
+++ scripts/install-lazy-vim.sh
@@ -26,9 +26,9 @@ fi
 if [[ ! -d /root/.config/nvim ]]; then
     git clone https://github.com/LazyVim/starter /root/.config/nvim
     rm -rf /root/.config/nvim/.git
-    echo "LazyVim starter cloned for root. ✅"
+    echo "✅ LazyVim starter cloned for root."
 else
-    echo "LazyVim starter already cloned for root. 🚧"
+    echo "⚠️ Warning: LazyVim starter already cloned for root."
 fi
 
 # Clone LazyVim starter for user.
@@ -36,9 +36,9 @@ if [[ ! -d /home/${USERNAME}/.config/nvim ]]; then
     git clone https://github.com/LazyVim/starter "/home/${USERNAME}/.config/nvim"
     rm -rf "/home/${USERNAME}/.config/nvim/.git"
     chown --recursive "${USERNAME}:${USERNAME}" "/home/${USERNAME}/.config/nvim"
-    echo "LazyVim starter cloned for user '${USERNAME}'. ✅"
+    echo "✅ LazyVim starter cloned for user '${USERNAME}'."
 else
-    echo "LazyVim starter already cloned for user '${USERNAME}'. 🚧"
+    echo "⚠️ Warning: LazyVim starter already cloned for user '${USERNAME}'."
 fi
 
 echo ""
