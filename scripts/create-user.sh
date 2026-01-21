7833241c1ee4e460ee7bb3f1f24cc4a13f641dbf
Tony Dang <tony@tonydang.com>
2026-01-20 20:46:53 -0800

    shellcheck

--- scripts/create-user.sh
+++ scripts/create-user.sh
@@ -5,6 +5,7 @@ echo "Creating user..."
 
 # Load environment variables. Required so that the USERNAME variable is available.
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# shellcheck disable=SC1091
 source "${SCRIPT_DIR}/../.env"
 
 # Only create a new user if one doesn't already exist.
@@ -12,19 +13,19 @@ if id "${USERNAME}" &>/dev/null; then
     echo "User '${USERNAME}' already exists. 🚧"
 else
     # Create a new user with home directory, and sudo privileges.
-    useradd --create-home --groups sudo ${USERNAME}
+    useradd --create-home --groups sudo "${USERNAME}"
     echo "User '${USERNAME}' created with sudo privileges."
 
     # Set the password to expire immediately, forcing the user to create a new
     # password on next login. Note: Only SSH key authentication will be allowed
     # for connecting to the server, but the password is still required for sudo.
-    passwd --delete --expire ${USERNAME}
+    passwd --delete --expire "${USERNAME}"
     echo "User '${USERNAME}' will need to create a new password on first login."
 
     # Copy SSH key folder from root to new user to allow SSH access. The SSH
     # folder contains the authorized_keys file, which contains the public key
     # that should have been added via the hosting provider on server creation.
-    rsync --archive --chown=${USERNAME}:${USERNAME} /root/.ssh /home/${USERNAME}
+    rsync --archive --chown="${USERNAME}:${USERNAME}" /root/.ssh "/home/${USERNAME}"
     echo "SSH public key copied from root to new user '${USERNAME}'."
 
     echo "User '${USERNAME}' created. ✅"
