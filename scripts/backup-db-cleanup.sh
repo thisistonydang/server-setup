a8fd90cbe08f5a30cb82e651c4e1dc66677a224f
Tony Dang <tony@tonydang.com>
2026-01-21 19:13:15 -0800

    move to .env

--- scripts/backup-db-cleanup.sh
+++ scripts/backup-db-cleanup.sh
@@ -6,17 +6,16 @@ echo "Cleaning up old database backups..."
 # Load environment variables. Required so that the following variables are available:
 # - POSTGRES_LOCAL_BACKUP_DIR
 # - POSTGRES_REMOTE_BACKUP_DIR
+# - POSTGRES_MIN_AGE_IN_DAYS
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck disable=SC1091
 source "${SCRIPT_DIR}/../.env"
 
-MIN_AGE_IN_DAYS=30
-
 # Clean local backups.
-find "${POSTGRES_LOCAL_BACKUP_DIR}" -type f -name "*.dump" -mtime "+${MIN_AGE_IN_DAYS}" -delete
+find "${POSTGRES_LOCAL_BACKUP_DIR}" -type f -name "*.dump" -mtime "+${POSTGRES_MIN_AGE_IN_DAYS}" -delete
 
 # Clean remote backups.
-rclone delete "${POSTGRES_REMOTE_BACKUP_DIR}" --include "*.dump" --min-age "${MIN_AGE_IN_DAYS}d" 
+rclone delete "${POSTGRES_REMOTE_BACKUP_DIR}" --include "*.dump" --min-age "${POSTGRES_MIN_AGE_IN_DAYS}d" 
 
 echo "✅ Database backups cleaned up."
 echo ""
